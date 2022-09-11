848feabc567fee9af111b8a8592365a2155ff12d
Mateo Gómez Fleytas <mateogf@hotmail.com>
2022-09-11 16:38:47 -0300

    calendario en js

--- mateo/script.js
+++ mateo/script.js
@@ -6,7 +6,10 @@ const btn = document.querySelector('[data-form-btn]');
 const createTask = (evento) => {
   evento.preventDefault();
   const input = document.querySelector('[data-form-input]');
+  const calendar = document.querySelector('[data-form-date]');
   const value = input.value;
+  const date = calendar.value;
+  console.log(date);
   const list = document.querySelector('[data-list]');
   const task = document.createElement('li');
   task.classList.add('card');
