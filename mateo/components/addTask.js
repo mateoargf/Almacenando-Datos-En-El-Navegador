ed8fb0140c073e2dedc6ed0b685ec76edc66db9c
Mateo Gómez Fleytas <mateogf@hotmail.com>
2022-10-04 15:41:02 -0300

    Creamos una variable para ordenar las nuevas tasks

--- mateo/components/addTask.js
+++ mateo/components/addTask.js
@@ -1,5 +1,6 @@
 import checkComplete from './checkComplete.js';
 import deleteIcon from './deleteIcon.js';
+import {  displayTasks  } from './readTasks.js'; 
 
 export const addTask = (evento) => {
   evento.preventDefault();
@@ -24,12 +25,13 @@ export const addTask = (evento) => {
     dateFormat
   }
 
+  list.innerHTML = '';
+
   const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
   taskList.push(taskObj);
   localStorage.setItem('tasks', JSON.stringify(taskList));
 
-  const task = createTask(taskObj);
-  list.appendChild(task);
+  displayTasks();
 }
 
 export const createTask = ({ value, dateFormat }) => {
--- mateo/components/readTasks.js
+++ mateo/components/readTasks.js
@@ -2,7 +2,7 @@ import { createTask } from './addTask.js';
 import { uniqueDates } from '../../services/date.js';
 import dateElement from './dateElement.js';
 
-export const readTasks = () => {
+export const displayTasks = () => {
     const list = document.querySelector("[data-list]");
     const tasksList = JSON.parse(localStorage.getItem('tasks')) || [];
     const dates = uniqueDates(tasksList);
