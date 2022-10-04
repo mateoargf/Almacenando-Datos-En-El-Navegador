23265a9858656536ee077819283e2c62a71635f0
Mateo Gómez Fleytas <mateogf@hotmail.com>
2022-10-04 19:56:56 -0300

    Elimine span de fechas(dateFormat)

--- mateo/components/addTask.js
+++ mateo/components/addTask.js
@@ -58,11 +58,7 @@ export const createTask = ({ value, dateFormat, complete, id }) => {
   taskContent.appendChild(check);
   taskContent.appendChild(titleTask);
 
-  const dateElement = document.createElement("span");
-  dateElement.innerHTML = dateFormat;
-
   task.appendChild(taskContent);
-  task.appendChild(dateElement);
   task.appendChild(deleteIcon(id));
   return task;
 };
